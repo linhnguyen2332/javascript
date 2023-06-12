d467c0f8285931c690a35f8d7b37172b6b56fd70
linhnguyen2332 <nhocxjnhhy1102@gmail.com>
2023-06-12 11:17:21 +0700

    rename function

--- bai4.6/findMax.js
+++ bai4.6/findMax.js
@@ -2,13 +2,14 @@
  * Write a program that takes a list of integers as input and returns the maximum product of any three distinct elements in the list
  * @param {Array} numbers
  */
-function findMaxProduct(numbers) {
-    const sortedNumbers = numbers.sort((a, b) => a - b);
-    const n = sortedNumbers.length;
+function findMaximumProductOfThreeDistincElement(numbers) {
+  const sortedNumbers = numbers.sort((a, b) => a - b);
+  const n = sortedNumbers.length;
 
-    const product1 = sortedNumbers[n - 1] * sortedNumbers[n - 2] * sortedNumbers[n - 3];
+  const product1 =
+    sortedNumbers[n - 1] * sortedNumbers[n - 2] * sortedNumbers[n - 3];
 
-    const product2 = sortedNumbers[0] * sortedNumbers[1] * sortedNumbers[n - 1];
-    return Math.max(product1, product2);
+  const product2 = sortedNumbers[0] * sortedNumbers[1] * sortedNumbers[n - 1];
+  return Math.max(product1, product2);
 }
-module.exports = findMaxProduct;
+module.exports = findMaximumProductOfThreeDistincElement;
